a5ec725ca40edb152b0b559dc468a4996b47d3b7
Matus Moravcik <matus@potatolondon.com>
2015-01-20 16:35:10 +0000

    Little PEP8-ing

--- osmosis/models.py
+++ osmosis/models.py
@@ -1,7 +1,6 @@
 import json
 import csv
 import StringIO
-import logging
 
 from django.db import models
 from django.db import connections
@@ -41,6 +40,7 @@ def transactional(func):
 
         return _wrapped
 
+
 class ImportStatus(object):
     PENDING = "pending"
     IN_PROGRESS = "in_progress"
@@ -48,20 +48,21 @@ class ImportStatus(object):
 
     @classmethod
     def choices(cls):
-        return ((ImportStatus.PENDING, "Pending"), (ImportStatus.IN_PROGRESS, "In Progress"), (ImportStatus.FINISHED, "Finished"))
+        return (ImportStatus.PENDING, "Pending"), (ImportStatus.IN_PROGRESS, "In Progress"), (ImportStatus.FINISHED, "Finished")
+
 
 class AbstractImportTask(models.Model):
     model_path = models.CharField(max_length=500, editable=False)
 
-    source_data = models.FileField("File", upload_to="/", max_length=1023) #FIXME: We should make upload_to somehow configurable
+    source_data = models.FileField("File", upload_to="/", max_length=1023)
 
-    error_csv = models.FileField("Error File", upload_to="/", editable=False, null=True, max_length=1023) #FIXME: Should make upload_to configurable
+    error_csv = models.FileField("Error File", upload_to="/", editable=False, null=True, max_length=1023)
     error_csv_filename = models.CharField(max_length=1023, editable=False)
 
     row_count = models.PositiveIntegerField(default=0, editable=False)
     shard_count = models.PositiveIntegerField(default=0, editable=False)
     shards_processed = models.PositiveIntegerField(default=0, editable=False)
-    # This field is here because we dont have foreign key relation anymore and
+    # This field is here because we don't have foreign key relation anymore and
     # retrieving this from ImportShard wasn't possible due to transaction
     shards_error_csv_written = models.BooleanField(default=False, editable=False)
 
@@ -84,7 +85,10 @@ class AbstractImportTask(models.Model):
 
     @classmethod
     def required_fields(cls):
-        """ Get a list of the required form fields from all of the forms in cls.Osmosis. """
+        """
+        Get a list of the required form fields from all of the
+        forms in cls.Osmosis.
+        """
         meta = cls.get_meta()
         fields = []
         for form in meta.forms:
@@ -95,7 +99,9 @@ class AbstractImportTask(models.Model):
 
     @classmethod
     def optional_fields(cls):
-        """ Get a list of the optional form fields from all of the forms in cls.Osmosis. """
+        """
+        Get a list of the optional form fields from all of the forms in cls.Osmosis.
+        """
         meta = cls.get_meta()
         fields = []
         for form in meta.forms:
@@ -106,7 +112,9 @@ class AbstractImportTask(models.Model):
 
     @classmethod
     def all_fields(cls):
-        """ Get an aggregate list of the form fields from all of the forms in cls.Osmosis. """
+        """
+        Get an aggregate list of the form fields from all of the forms in cls.Osmosis.
+        """
         meta = cls.get_meta()
         fields = []
         for form in meta.forms:
@@ -116,19 +124,21 @@ class AbstractImportTask(models.Model):
 
     @classmethod
     def get_meta(cls):
-        """ Get the info from self.Osmosis (where self can be a subclass), using defaults from
-            the parent ImportTask.Osmosis for values which are not defined on SubClass.Osmosis.
+        """
+        Get the info from self.Osmosis (where self can be a subclass),
+        using defaults from the parent ImportTask.Osmosis for values
+        which are not defined on SubClass.Osmosis.
         """
         meta = getattr(cls, "Osmosis")
 
         if not hasattr(meta, "_initialised"):
 
-            for attr in ( x for x in dir(AbstractImportTask.Osmosis) if not x.startswith("_") ):
+            for attr in (x for x in dir(AbstractImportTask.Osmosis) if not x.startswith("_")):
                 if not hasattr(meta, attr):
                     setattr(meta, attr, getattr(AbstractImportTask.Osmosis, attr))
 
-            #If we were given any forms by their module path, then swap them here
-            #so that meta.forms is always a list of classes
+            # If we were given any forms by their module path, then swap
+            # them here so that meta.forms is always a list of classes
             new_forms = []
             for form in meta.forms:
                 if isinstance(form, basestring):
@@ -147,29 +157,29 @@ class AbstractImportTask(models.Model):
         deferred.defer(kallable, *args, **kwargs)
 
     def start(self):
-        self.save()  #Make sure we are saved before processing
+        self.save()  # Make sure we are saved before processing
 
         self.row_columns = None
         self.defer(self.process)
 
     def next_source_row(self, handle):
         """
-            Given a file handle, return the next row of data as a key value dict.
+        Given a file handle, return the next row of data as a key value dict.
 
-            Return None to denote the EOF
-            Return False to skip this row of data entirely
+        Return None to denote the EOF
+        Return False to skip this row of data entirely
         """
-        line = handle.readline()  #By default, assume CSV
+        line = handle.readline()  # By default, assume CSV
 
         if not line:
             return None
 
         if not line.strip():
-            #Skip lines with just whitespace
+            # Skip lines with just whitespace
             return False
 
         if not getattr(self, "detected_dialect", None):
-            #Sniff for the dialect of the CSV file
+            # Sniff for the dialect of the CSV file
 
             pos = handle.tell()
             handle.seek(0)
@@ -179,7 +189,7 @@ class AbstractImportTask(models.Model):
             try:
                 dialect = csv.Sniffer().sniff(readahead, ",")
             except csv.Error:
-                #Fallback to excel format
+                # Fallback to excel format
                 dialect = csv.excel
 
             dialect_attrs = [
@@ -192,13 +202,13 @@ class AbstractImportTask(models.Model):
                 "skipinitialspace"
             ]
 
-            self.detected_dialect = { x: getattr(dialect, x) for x in dialect_attrs }
+            self.detected_dialect = {x: getattr(dialect, x) for x in dialect_attrs}
 
         reader = csv.reader(StringIO.StringIO(line), **self.detected_dialect)
 
         if not getattr(self, "detected_columns", None):
-            #On first iteration, the line will be the column headings, store those
-            #and return False to skip processing
+            # On first iteration, the line will be the column headings,
+            # store those and return False to skip processing
             columns = reader.next()
             self.detected_columns = columns
             return False
@@ -206,10 +216,10 @@ class AbstractImportTask(models.Model):
         cols = self.detected_columns
         values = reader.next()
 
-        return { x: values[i] for i, x in enumerate(cols) }
+        return {x: values[i] for i, x in enumerate(cols)}
 
     def process(self):
-        #Reload, we've been pickled in'it
+        # Reload, we've been pickled in'it
         self = self.__class__.objects.get(pk=self.pk)
         self.status = ImportStatus.IN_PROGRESS
 
@@ -220,18 +230,19 @@ class AbstractImportTask(models.Model):
         lineno = 0
 
         while True:
-            lineno += 1  #Line numbers are 1-based
+            lineno += 1  # Line numbers are 1-based
             data = self.next_source_row(uploaded_file)
 
             if data is False:
                 # Skip this row
                 continue
             elif data:
-                shard_data.append(data)  #Keep a buffer of the data to process in this shard
+                shard_data.append(data)  # Keep a buffer of the data to process in this shard
 
             data_length = len(shard_data)
             if shard_data and (data_length == meta.rows_per_shard or data is None):
-                #If we hit the predefined shard count, or the EOF of the file then process what we have
+                # If we hit the predefined shard count, or the EOF of the
+                # file then process what we have
 
                 new_shard = ImportShard.objects.create(
                     task_id=self.pk,
@@ -249,7 +260,7 @@ class AbstractImportTask(models.Model):
                 shard_data = []
 
             if not data:
-                #Break at the end of the file
+                # Break at the end of the file
                 break
 
         # 2 == HEADER + 1-based to 0-based
@@ -260,7 +271,7 @@ class AbstractImportTask(models.Model):
 
     def import_row(self, forms, cleaned_data):
         """
-            Called when a row of source data is found to be valid and is ready for saving
+        Called when a row of source data is found to be valid and is ready for saving
         """
         raise NotImplementedError()
 
@@ -274,7 +285,7 @@ class AbstractImportTask(models.Model):
 
     def finish(self):
         """
-            Called when all shards have finished processing
+        Called when all shards have finished processing
         """
         if self.get_meta().generate_error_csv:
             self.error_csv_filename = self._error_csv_filename()
@@ -315,7 +326,7 @@ class AbstractImportTask(models.Model):
             self.shards_processed == self.shard_count,
             not self.shards_error_csv_written,
         ]):
-            #Defer the finish callback when we've processed all shards
+            # Defer the finish callback when we've processed all shards
             self.status = ImportStatus.FINISHED
             defer_finish = True
 
@@ -331,7 +342,7 @@ class AbstractImportTask(models.Model):
 
 class ModelImportTaskMixin(object):
     def import_row(self, forms, cleaned_data):
-        return [ form.save() for form in forms ]
+        return [form.save() for form in forms]
 
 
 class ImportShard(models.Model):
@@ -359,19 +370,19 @@ class ImportShard(models.Model):
         meta = self.task.get_meta()
         task_model = get_model(*self.task.model_path.split("."))
 
-        this = ImportShard.objects.get(pk=self.pk)  #Reload, self is pickled
+        this = ImportShard.objects.get(pk=self.pk)  # Reload, self is pickled
         source_data = json.loads(this.source_data_json)
 
-        #If there are no rows to process
+        # If there are no rows to process
         mark_shard_complete = this.last_row_processed == this.total_rows - 1 or this.total_rows == 0
 
-        for i in xrange(this.last_row_processed, this.total_rows):  #Always continue from the last processed row
+        for i in xrange(this.last_row_processed, this.total_rows):  # Always continue from the last processed row
             data = source_data[i]
 
-            forms = [ self.task.instantiate_form(form, data) for form in meta.forms ]
+            forms = [self.task.instantiate_form(form, data) for form in meta.forms]
 
-            if all([ form.is_valid() for form in forms ]):
-                #All forms are valid, let's process this shizzle
+            if all([form.is_valid() for form in forms]):
+                # All forms are valid, let's process this shizzle
 
                 cleaned_data = {}
                 for form in forms:
@@ -380,7 +391,7 @@ class ImportShard(models.Model):
                 try:
                     self.task.import_row(forms, cleaned_data)
                 except ValidationError, e:
-                    #We allow subclasses to raise a validation error on import_row
+                    # We allow subclasses to raise a validation error on import_row
                     errors = []
                     if hasattr(e, 'message_dict'):
                         for name, errs in e.message_dict.items():
@@ -402,7 +413,7 @@ class ImportShard(models.Model):
 
                 self.handle_error(this.start_line_number + i, data, errors)
 
-            #Now update the last processed row, transactionally
+            # Now update the last processed row, transactionally
             @transactional
             def update_shard(_this):
                 _this = ImportShard.objects.get(pk=_this.pk)
@@ -411,8 +422,8 @@ class ImportShard(models.Model):
                 return _this
 
             this = update_shard(this)
-
-            mark_shard_complete = i == this.total_rows - 1 #If this was the last iteration then mark as complete
+            # If this was the last iteration then mark as complete
+            mark_shard_complete = i == this.total_rows - 1
 
         if mark_shard_complete:
             @transactional
@@ -438,10 +449,9 @@ class ImportShard(models.Model):
         if not get_model(*self.task.model_path.split(".")).get_meta().generate_error_csv:
             return
 
-        cols = getattr(self.task, "detected_columns", sorted(data.keys())) + [ "errors" ]
         ImportShardError.objects.create(
             shard=self,
-            line=json.dumps(data.values() + [ ". ".join(errors) ])
+            line=json.dumps(data.values() + [". ".join(errors)])
         )
 
     def _error_csv_filename(self):
