f3d371b47d5abfa6894a823cdff20483c9f58091
Matus Moravcik <matus@potatolondon.com>
2015-01-21 14:17:16 +0000

    Add migration for model changes

---
+++ osmosis/migrations/0004_auto__add_field_importtask_shards_error_csv_written__del_field_imports.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'ImportTask.shards_error_csv_written'
+        db.add_column(u'osmosis_importtask', 'shards_error_csv_written',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+        # Deleting field 'ImportShard.task'
+        db.delete_column(u'osmosis_importshard', 'task_id')
+
+        # Adding field 'ImportShard.task_model_path'
+        db.add_column(u'osmosis_importshard', 'task_model_path',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=500),
+                      keep_default=False)
+
+        # Adding field 'ImportShard.task_id'
+        db.add_column(u'osmosis_importshard', 'task_id',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'ImportTask.shards_error_csv_written'
+        db.delete_column(u'osmosis_importtask', 'shards_error_csv_written')
+
+
+        # User chose to not deal with backwards NULL issues for 'ImportShard.task'
+        raise RuntimeError("Cannot reverse this migration. 'ImportShard.task' and its values cannot be restored.")
+        
+        # The following code is provided here to aid in writing a correct migration        # Adding field 'ImportShard.task'
+        db.add_column(u'osmosis_importshard', 'task',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['osmosis.ImportTask']),
+                      keep_default=False)
+
+        # Deleting field 'ImportShard.task_model_path'
+        db.delete_column(u'osmosis_importshard', 'task_model_path')
+
+        # Deleting field 'ImportShard.task_id'
+        db.delete_column(u'osmosis_importshard', 'task_id')
+
+
+    models = {
+        u'osmosis.importshard': {
+            'Meta': {'object_name': 'ImportShard'},
+            'complete': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'error_csv_filename': ('django.db.models.fields.CharField', [], {'max_length': '1023'}),
+            'error_csv_written': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_row_processed': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'source_data_json': ('django.db.models.fields.TextField', [], {}),
+            'start_line_number': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'task_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'task_model_path': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'total_rows': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'})
+        },
+        u'osmosis.importsharderror': {
+            'Meta': {'object_name': 'ImportShardError'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'line': ('django.db.models.fields.TextField', [], {}),
+            'shard': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['osmosis.ImportShard']"})
+        },
+        u'osmosis.importtask': {
+            'Meta': {'object_name': 'ImportTask'},
+            'error_csv': ('django.db.models.fields.files.FileField', [], {'max_length': '1023', 'null': 'True'}),
+            'error_csv_filename': ('django.db.models.fields.CharField', [], {'max_length': '1023'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model_path': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'row_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'shard_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'shards_error_csv_written': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'shards_processed': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'source_data': ('django.db.models.fields.files.FileField', [], {'max_length': '1023'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'pending'", 'max_length': '32'})
+        }
+    }
+
+    complete_apps = ['osmosis']
