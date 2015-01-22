b3f418f1dce270b489ea62f38fa6c43b94337288
Matus Moravcik <matus@potatolondon.com>
2015-01-22 18:11:58 +0000

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+PACKAGES = find_packages()
+
+setup(
+    name='Osmosis',
+    version='1.0',
+    description='A Django app for importing files into a database on the Google AppEngine platform ',
+    author='Potato London Ltd',
+    url='https://github.com/potatolondon/osmosis',
+    packages=PACKAGES,
+    include_package_data=True,
+    install_requires=[
+        'GoogleAppEngineCloudStorageClient',
+    ],
+)
